a1605233d0f17bcd7349da0ca5a9c06e34cf7cc0
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-02-28 01:17:12 +0530

    js logic updated

--- app.js
+++ app.js
@@ -5,28 +5,32 @@ var output=document.querySelector("#output");
 const btnSubmit=document.querySelector("#submitBtn");
 
 
+function calculateProfitAndLoss(initialPrice,quantity,currentPrice){
+    if(initialPrice>currentPrice){
+        var loss= (initialPrice-currentPrice)*quantity;
+         var lossPercent= (loss/initialPrice)*100;
+         output.style.color="red";
+         output.innerText=`Hey, the loss is ${Math.abs(loss)} and the percent is ${lossPercent.toFixed(2)}%`;
 
-function clickHandler(){
-    var spending= initialPrice*quantity;;
-    var afterPrice= currentPrice*quantity;
-    
-    if (initialPrice !== '' && quantity !== '' && currentPrice !== ''){
-        var result=afterPrice-spending;
-        if(result>=0){
-            output.style.color="green";
-            var profitPercent=result/spending * 100;
-            output.innerText=`Hey, the profit is ${result} and the percent is ${profitPercent.toFixed(2)}%`;
-        }
-        if(result<0){
-            output.style.color="red";
-            var lossPercent=result/spending * 100;
-            output.innerText=`Hey, the loss is ${Math.abs(result)} and the percent is ${Math.abs(lossPercent.toFixed(2))}%`;
-
-        }
-       
-
-        
     }
+    else if(currentPrice>initialPrice){
+        var profit= (currentPrice-initialPrice)*quantity;
+         var profitPercent= (profit/initialPrice)*100;
+         output.style.color="green";
+         output.innerText=`Hey, the profit is ${profit} and the percent is ${profitPercent.toFixed(2)}%`;
+
+    }
+    else{
+        output.innerText=`Hey, you have neither made a profit nor a loss.`;
+
+    }
+
+}
+function clickHandler(){
+    var ip= Number(initialPrice);
+    var qty= Number(quantity);
+    var cp= Number(currentPrice);
+    calculateProfitAndLoss(ip, qty, cp);
 }
 
 btnSubmit.addEventListener("click",clickHandler);
